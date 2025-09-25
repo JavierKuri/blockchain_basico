ed205f1ec00bcef3266ffce38abeeaaf3239212b
JKS <javier.kuri@anahuac.mx>
2025-09-25 13:37:07 -0600

    Added initial block class

---
+++ block.py
@@ -0,0 +1,9 @@
+import hashlib
+import time
+
+class Block:
+    def __init__(self, previous_hash: str, content: str):
+        self.previous_hash = previous_hash
+        self.timestamp = time.time()
+        self.content = content
+        self.hash = hashlib.sha256(f"{self.previous_hash}{self.timestamp}{self.content}".encode()).hexdigest()
